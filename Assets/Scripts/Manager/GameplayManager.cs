68939e4cd72df30b929d0ca4dadbb63387132a4a
Pham Van Dong <phamvandongg18012000@gmail.com>
2023-08-07 18:04:18 +0700

    Fix

--- Assets/Scripts/Manager/GameplayManager.cs
+++ Assets/Scripts/Manager/GameplayManager.cs
@@ -12,20 +12,27 @@ public class GameplayManager : Singleton<GameplayManager>
 
     [SerializeField] List<Transform> listSpawnPosition;
 
+    Vector3 spawnPosition;
+
 
     public void SpawnUnits(int unitCount)
     {
         for (int i = 0; i < unitCount; i++)
         {
-            StartCoroutine(Spawn(5f));
-            int rand = Random.Range(0, listSpawnPosition.Count);
-            var spiderObj = PoolObjects.Ins.Spawn(spiderPrefab, listSpawnPosition[rand]);
+            StartCoroutine(Spawn(1f));
         }
     }
 
     private IEnumerator Spawn(float delay)
     {
+        Spider spiderObj = PoolObjects.Ins.Spawn(spiderPrefab, GenerateSpawnPosition());
         yield return new WaitForSeconds(delay);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        int rand = Random.Range(0, listSpawnPosition.Count);
+        return listSpawnPosition[rand].position;
     }
 
     public Vector3 GetGoalPosition()
